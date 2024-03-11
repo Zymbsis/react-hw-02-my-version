fee68d12a595b67d58e544eded5c6f9b62194dde
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-03-11 22:27:37 +0100

    update

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,13 +1,17 @@
 import css from './Feedback.module.css';
-// import { useState } from 'react';
-const Feedback = ({ children, value }) => {
+
+const Feedback = ({ clicks }) => {
   return (
-    <div className={css.optionsWrapper}>
-      <span>
-        {children}
-        {value}
-      </span>
-    </div>
+    <ul className={css.ratingWrapper}>
+      {Object.entries(clicks).map((item, index) => {
+        return (
+          <li key={index}>
+            <span>{`${item[0]}: ${item[1]}`}</span>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
+
 export default Feedback;
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,10 +1,58 @@
 import css from './Options.module.css';
+import { useState, useEffect } from 'react';
+import Feedback from '../Feedback/Feedback';
+import Notification from '../Notification/Notification';
 
-const Options = ({ children, handleClick }) => {
+const Options = () => {
+  const rating = JSON.parse(localStorage.getItem('rating'));
+  const initialRating = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+    positive: 0,
+  };
+
+  const [clicks, setClicks] = useState(rating ? rating : initialRating);
+
+  const updateRating = type => {
+    setClicks(prevState => {
+      if (type === 'Reset') {
+        return initialRating;
+      } else {
+        const newClicks = {
+          ...prevState,
+          [type === 'Good' ? 'good' : type === 'Neutral' ? 'neutral' : 'bad']:
+            prevState[type.toLowerCase()] + 1,
+        };
+        newClicks.total = newClicks.good + newClicks.neutral + newClicks.bad;
+        newClicks.positive =
+          Math.round(
+            ((newClicks.good + newClicks.neutral) / newClicks.total) * 100
+          ) + '%';
+        return newClicks;
+      }
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem('rating', JSON.stringify(clicks));
+  }, [clicks]);
+
+  const handleClick = e => {
+    updateRating(e.target.textContent);
+  };
+
   return (
-    <div className={css.buttonWrapper}>
-      <button onClick={handleClick}>{children}</button>
-    </div>
+    <>
+      <div className={css.buttonWrapper}>
+        <button onClick={handleClick}>Good</button>
+        <button onClick={handleClick}>Neutral</button>
+        <button onClick={handleClick}>Bad</button>
+        {clicks.total > 0 && <button onClick={handleClick}>Reset</button>}
+      </div>
+      {clicks.total ? <Feedback clicks={clicks} /> : <Notification />}
+    </>
   );
 };
 export default Options;
